f03abcadcb97439539a5a74c6d0dbc372b10598f
Henri Malkki <henri.malkki@cs.helsinki.fi>
2015-02-08 23:47:47 +0200

    t14

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,5 +1,6 @@
 class BreweriesController < ApplicationController
   before_action :set_brewery, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_that_signed_in, except: [:index, :show]  
  # before_filter :authenticate, only: [:destroy]
 
   # GET /breweries
